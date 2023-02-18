3bde30fa03c45f230864937e90b32da586a9665e
Ricardo Morales Dominguez <rmorales8303@gmail.com>
2023-02-18 14:49:49 -0500

    Uso básico de print y println

--- TFT_BasicTutorial_230218.ino
+++ TFT_BasicTutorial_230218.ino
@@ -1,9 +1,17 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <TFT_HX8357.h>  // Hardware-specific library
+TFT_HX8357 tft = TFT_HX8357();  // Invoke custom library
 
+void setup() {  
+  tft.init();
+  tft.setRotation(1); //5%4=1
+  tft.setTextSize(4);
+  tft.fillScreen(TFT_BLACK);  
+  tft.println("Hello world");
+  tft.println("Como estas");
+  tft.setTextSize(2);
+  tft.println("Muy bien");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
 }
